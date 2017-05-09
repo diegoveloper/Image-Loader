f4e75dbdb013a2525699d83e908251c579ce076b
diegoveloper <diego.velasquez.lopez@gmail.com>
2017-05-09 10:42:24 -0500

    Initial Commit

---
+++ ImageLoaderProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageLoaderProject
+//
+//  Created by Diego Enrique Velasquez Lopez on 5/9/17.
+//  Copyright © 2017 Diego Enrique Velasquez Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
